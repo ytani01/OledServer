1770106af80180cc90f6bc1cd4f0be07988400dd
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-12-31 06:45:21 +0900

    update

--- Template.py
+++ Template.py
@@ -2,16 +2,13 @@
 #
 # (C) 2018 Yoichi Tanibayashi
 #
-import telnetlib
-import time
 import click
 
 from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO, WARN
 logger = getLogger(__name__)
+logger.setLevel(DEBUG)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
-#handler_fmt = Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 handler_fmt = Formatter('%(asctime)s %(levelname)s %(funcName)s> %(message)s',
                         datefmt='%H:%M:%S')
 handler.setFormatter(handler_fmt)
@@ -19,19 +16,43 @@ logger.addHandler(handler)
 logger.propagate = False
 
 #####
-class OledClient:
-    DEF_HOST = 'localhost'
+class MainClass:
+    DEF_SERVER = 'localhost'
     DEF_PORT = 12345
 
     def __init__(self, server='', port=0):
-        pass
+        self.server, self.port = __class__.DEF_SERVER, __class__.DEF_PORT
+        if server != '':
+            self.server = server
+        if port != 0:
+            self.port = port
+
+    def func1(self, text):
+        logger.debug('text=\'%s\'', text)
+        
+        logger.info('%s:%d', self.server, self.port)
+        print(text)
+        
     
 #####
-@click.command(help='OLED client')
-@click.argument('server', type=str, default='')
-@click.argument('port', type=int, default=0)
-def main(server, port):
-    cl = OledClient(server, port)
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS, help='Template program')
+@click.argument('text', type=str, default='Hello world !')
+@click.option('--server', '-s', 'server', type=str, default='localhost',
+              help='server\'s hostname or IP address')
+@click.option('--port', '-p', 'port', type=int, default=0,
+              help='server\'s port number')
+@click.option('--debug', '-d', 'debug', is_flag=True, default=False,
+              help='debug flag')
+def main(text, server, port, debug):
+    logger.setLevel(INFO)
+    if debug:
+        logger.setLevel(DEBUG)
+
+    logger.debug('text=\'%s\', server=\'%s\', port=%d', text, server, port)
+    
+    obj = MainClass(server, port)
+    obj.func1(text)
 
 if __name__ == '__main__':
     main()
