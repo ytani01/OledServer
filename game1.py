9320fa40cbd20be6bde59766b73f400b9039a656
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-02-03 05:58:02 +0900

    update

--- game1.py
+++ game1.py
@@ -83,53 +83,75 @@ class Bar:
         self.lock.release()
 
 class Ball:
-    def __init__(self, ol, bar, color, xy, r, vxy=(0, 0), debug=False):
+    def __init__(self, ol, bar, color, xy, r, v=(0, 0), debug=False):
         self.logger = init_logger(__class__.__name__, debug)
         self.debug = debug
         self.logger.debug('color = %s', str(color))
         self.logger.debug('xy    = %s', xy)
         self.logger.debug('r     = %d', r)
-        self.logger.debug('vxy   = %s', vxy)
+        self.logger.debug('v     = %s', v)
 
         self.ol            = ol
         self.bar           = bar
         self.color         = color
         (self.x, self.y)   = xy
         self.r             = r
-        (self.vx, self.vy) = vxy
+        (self.vx, self.vy) = v
 
         self.lock = threading.Lock()
+
+    def check_frame(self):
+        (x1,  y1)  = (self.x,   self.y)
+        (vx1, vy1) = (self.vx,  self.vy)
+        
+        (x2,  y2)  = (x1 + vx1, y1 + vy1)
+        (vx2, vy2) = (vx1,      vy1)
+
+        (x_min, y_min) = (0, 0)
+        (x_max, y_max) = (self.ol.disp.width - 1, self.ol.disp.height -1)
+
+        if x2 < 0:
+            x2 = -x2
+            vx2 = -vx1
+        if x2 > x_max:
+            x2 = 2 * x_max - x2
+            vx2 = -vx1
+
+        if y2 < 0:
+            y2 = -y2
+            vy2 = -vy1
+        if y2 > y_max:
+            y2 = 2 * y_max - y2
+            vy2 = -vy1
+
+        return ((x2, y2), (vx2, vy2))
 
     def move(self):
         self.lock.acquire()
 
-        x2 = self.x + self.vx
-        y2 = self.y + self.vy
+        (x1,  y1)  = (self.x,   self.y)
+        (vx1, vy1) = (self.vx,  self.vy)
+        
+        (x2,  y2)  = (x1 + vx1, y1 + vy1)
+        (vx2, vy2) = (vx1,      vy1)
 
         # frame
-        if x2 < 0:
-            x2 = -x2
-            self.vx = -self.vx
-        if x2 > self.ol.disp.width - 1:
-            x2 = 2 * self.ol.disp.width - 2 - x2
-            self.vx = -self.vx
-        if y2 < 0:
-            y2 = -y2
-            self.vy = -self.vy
-        if y2 > self.ol.disp.height -1:
-            y2 = 2 * self.ol.disp.height - 2 - y2
-            self.vy = -self.vy
+        ((x2, y2), (vx2, vy2)) = self.check_frame()
 
         # bar
-        if self.y <= self.bar.y and y2 >= self.bar.y:
-            xb = (x2 - self.x) * (self.bar.y - self.y) / (y2 - self.y) + self.x
-            if abs(xb - self.bar.x) <= self.bar.l / 2:
-                # shoot!
-                self.logger.debug('|xb - bar.x| = %f', abs(xb - self.bar.x))
+        (bar_x, bar_y) = (self.bar.x, self.bar.y)
+        bar_l          = self.bar.l
+        
+        if y1 <= bar_y and y2 >= bar_y:
+            xb = (x2 - x1) * (bar_y - y1) / (y2 - y1) + x1
 
-                y2 = 2 * self.bar.y - y2
-                vx2 = self.vx
-                vy2 = -self.vy
+            if abs(xb - bar_x) <= bar_l / 2:
+                # shoot!
+                self.logger.debug('|xb - bar_x| = %f', abs(xb - bar_x))
+
+                y2 = 2 * bar_y - y2
+                vx2 = vx1
+                vy2 = -vy1
 
                 dy = 0.7
                 dx = 1.0
@@ -138,29 +160,29 @@ class Ball:
                 vy_min = 0.5
                 vy_max = 6.0
 
-                if abs(xb - self.bar.x) <= 2:
+                if abs(xb - bar_x) <= 2:
                     self.logger.debug('A:vy2 = %f', vy2)
                     vy2 -= dy
                     
-                if xb > self.bar.x + self.bar.l / 2 - self.bar.l * 0.2:
-                    vx2 = self.vx + dx
+                if xb > bar_x + bar_l / 2 - bar_l * 0.2:
+                    vx2 = vx1 + dx
                     vy2 += dy
                     self.logger.debug('B:vx2,vy2 = %f,%f', vx2, vy2)
-                    if self.vx > 0 and vx2 > vx_max:
+                    if vx1 > 0 and vx2 > vx_max:
                         vx2 = vx_max
                         self.logger.debug('C:vx2,vy2 = %f,%f', vx2, vy2)
-                    if self.vx < 0 and vx2 > -vx_min:
+                    if vx1 < 0 and vx2 > -vx_min:
                         vx2 = -vx_min
                         self.logger.debug('D:vx2,vy2 = %f,%f', vx2, vy2)
                     
-                if xb < self.bar.x - self.bar.l / 2 + self.bar.l * 0.2:
-                    vx2 = self.vx - dx
+                if xb < bar_x - bar_l / 2 + bar_l * 0.2:
+                    vx2 = vx1 - dx
                     vy2 += dy
                     self.logger.debug('E:vx2,vy2 = %f,%f', vx2, vy2)
-                    if self.vx > 0 and vx2 < vx_min:
+                    if vx1 > 0 and vx2 < vx_min:
                         vx2 = vx_min
                         self.logger.debug('F:vx2,vy2 = %f,%f', vx2, vy2)
-                    if self.vx < 0 and vx2 < -vx_max:
+                    if vx1 < 0 and vx2 < -vx_max:
                         vx2 = -vx_max
                         self.logger.debug('G:vx2,vy2 = %f,%f', vx2, vy2)
                     
@@ -176,8 +198,9 @@ class Ball:
                     
                 self.logger.debug('v = (%f, %f)', self.vx, self.vy)
 
-        self.x = x2
-        self.y = y2
+        (self.x,  self.y)  = (x2,  y2)
+        (self.vx, self.vy) = (vx2, vy2)
+        
 
         self.lock.release()
 
@@ -232,6 +255,15 @@ class App:
             self.ball[i].draw()
         self.ol.display()
 
+    def main(self):
+        disp_th = threading.Thread(target=self.move, daemon=True)
+        disp_th.start()
+
+        while True:
+            self.draw()
+            self.ol.display()
+            time.sleep(0.01)
+                
     def cb_re(self, val):
         self.logger.debug('val = %s', RotaryEncoder.val2str(val))
 
@@ -247,25 +279,17 @@ class App:
 
     def move(self):
         while True:
-            for i in range(len(self.ball)):
-                self.ball[i].move()
+            for b in self.ball:
+                b.move()
+                
             time.sleep(.1)
 
     def draw(self):
         self.frame.draw()
-        for i in range(len(self.ball)):
-            self.ball[i].draw()
+        for b in self.ball:
+            b.draw()
         self.bar.draw()
 
-    def main(self):
-        disp_th = threading.Thread(target=self.move, daemon=True)
-        disp_th.start()
-
-        while True:
-            self.draw()
-            self.ol.display()
-            time.sleep(0.01)
-                
     def finish(self):
         self.logger.debug('')
         self.ol.cleanup()
